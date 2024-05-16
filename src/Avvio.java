9902170d38f4242b0e8c86cf71165698922ec662
Pakato727 <146851784+Pakato727@users.noreply.github.com>
2024-05-16 21:25:38 +0200

    master

--- src/Avvio.java
+++ src/Avvio.java
@@ -2,7 +2,23 @@
 public class Avvio {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		TestMetodi test = new TestMetodi();
+		test.stampa();
+		test.somma(3, 5);
+		test.schedaUtente("lalal", "ulolo", 31);
+		
+		System.out.println(test.sommaRitornata(10,15));
+		System.out.println(test.sommaStringata(10,15));
+		//oppure
+		//Integer sommaRit = test.sommaRitornata(10,15);
+		//Integer sommaStr = test.sommaStringata(10,15);
+        //System.out.println(sommaRit);
+        //System.out.println(sommaStr);
+		
+		System.out.println(TestMetodi.dammiLunghezzaStr("palala"));
+		
+		System.out.println(TestMetodi.verificaUser("alalalala11")); //true
+		System.out.println(TestMetodi.verificaUser("al")); //false
 
 	}
 
--- src/TestMetodi.java
+++ src/TestMetodi.java
@@ -1,4 +1,42 @@
 
 public class TestMetodi {
-
+	void stampa() {
+		System.out.println("Ciao Mondo!");
+	}
+	
+	void somma(int n1, int n2) {
+		int risultato = n1+n2;
+		System.out.println("La somma è: " + risultato);
+	}
+	
+	void schedaUtente(String nome, String cognome, int eta) {
+		System.out.println("Nome: " + nome);
+		System.out.println("Cognome: " + cognome);
+		System.out.println("Età: " + eta);	
+	}
+	
+	Integer sommaRitornata(int n1, int n2) {
+		return n1+n2;
+	}
+	
+	String sommaStringata(int n1, int n2) {
+		return "la somma è: "+(n1+n2);
+	}
+	
+	static Integer dammiLunghezzaStr(String nome) {
+		return nome.length();
+	}
+	
+	
+	//verificare che lo username abbia una lunghezza compresa tra 3 e i 12 caratteri,
+	//ritorna true se la verifica è andata a buon fine
+	static Boolean verificaUser(String username) {
+		if(username.length()>=3 && username.length()<=12) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	}
 }
